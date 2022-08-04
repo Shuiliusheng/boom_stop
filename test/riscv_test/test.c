1b9759acf58c4d9d16a27e5e51f1ed98ef5981d4
Shuiliusheng <cuihongwei@pku.edu.cn>
2022-08-04 22:50:46 +0800

    update the test/riscv_test

--- test/riscv_test/test.c
+++ test/riscv_test/test.c
@@ -1,80 +1,37 @@
-#include "test.h"
-#define GetNPC(npc) asm volatile( \
-    "addi x0, t0, 1027 \n\t"  \
-    "mv %[npc], t0  # uretaddr \n\t"  \
-    : [npc]"=r"(npc)\
-    :  \
-); 
+#include "define.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 
-#define SetNPC(npc) asm volatile( \
-    "mv t0, %[npc]  # uretaddr \n\t"  \
-    "addi x0, t0, 5 \n\t"  \
-    : \
-    :[npc]"r"(npc)  \
-); 
-#define URet() asm volatile( \
-    "addi x0, x0, 128  # uret \n\t"  \
-); 
-
-
-unsigned long long intregs[32];
-unsigned long long necessaryRegs[1000];
-
-unsigned long long npc=0;
-unsigned long long exittime=0;
-unsigned long long tag=0x1234567;
-unsigned long long exitFucAddr=0x80002008;
-unsigned long long maxinst=1000;
-
-char str[300];
-
-void exit_fuc()
-{
-    Save_int_regs();
-    Load_necessary();
-
-    GetNPC(npc);
-    SetNPC(npc);
-    exittime++;
-
-    endcycle = __csrr_cycle();
-    endinst = __csrr_instret();
-    // sprintf(str, "exit function! %ld %ld\n", endcycle, endinst);
-    // printstr(str);
-    printf("exit function! %ld %ld\n", endcycle, endinst);
-    printf("exit function! %ld %ld\n", endcycle, endinst);
-    printf("exit function! %ld %ld\n", endcycle, endinst);
-    printf("exit function! %ld %ld\n", endcycle, endinst);
-    printf("exit function! %ld %ld\n", endcycle, endinst);
-
-    SetCtrlReg(tag, exitFucAddr, maxinst);
-    Load_int_regs();
-    URet();
-}
-
+uint64_t procTag=0x1234567;
+uint64_t maxevent=0, warmupinst=0, eventsel=0;
+uint64_t sampleFuncAddr = 0;
+uint64_t hpcounters[64];
+uint64_t tempStackMem[4096];
 
 int main()
 {
-    unsigned long long t1 = (unsigned long long)&intregs[0];
-    unsigned long long t2 = (unsigned long long)&necessaryRegs[0];
-    unsigned long long t3 = 0;
-    necessaryRegs[0]=(unsigned long long)&necessaryRegs[400];
-    printf("t1: 0x%lx, t2: 0x%lx\n", t1, t2);
-    SetTempReg(t1, t2, t3);
-    Save_necessary();
+    SetSampleBaseInfo(procTag, sampleFuncAddr);
+    SetSampleCtrlReg(maxevent, warmupinst, eventsel);
+    SetCounterLevel("3");
+    SetPfcEnable(1);
 
-    startcycle = __csrr_cycle();
-    startinst = __csrr_instret();
-    SetCtrlReg(tag, exitFucAddr, maxinst);
-
-    // printf("hello world\n");
+    uint64_t t1 = 1, t2 = 2, t3 = 3;
     for(int i=0;i<10000;i++){
-        // printf("hello world: %d\n", i);
         t1 = t2 + t3;
         t2 = t2 + t3;
         t3 = t2 + t3;
+        tempStackMem[i%4096] = t1 + t2 + t3;
     }
-    intregs[0]=t1;
-    // printf("exittime: %d\n", exittime);
+
+    ReadCounter16(&hpcounters[0], 0);
+	ReadCounter16(&hpcounters[16], 16);
+	ReadCounter16(&hpcounters[32], 32);
+	ReadCounter16(&hpcounters[48], 48);
+
+	for(int n=0;n<64;n++){
+		printf("{\"type\": \"event %2d\", \"value\": %llu}\n", n, hpcounters[n]);
+    }
+
     return 0;
 }
