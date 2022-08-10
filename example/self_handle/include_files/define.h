73255d5d824b941560f58557bcc97487e3cacd40
Shuiliusheng <cuihongwei@pku.edu.cn>
2022-08-10 22:49:36 +0800

    fixed a bug about setting sample config in example/self_handle codes

--- example/self_handle/include_files/define.h
+++ example/self_handle/include_files/define.h
@@ -112,7 +112,7 @@
     "mv t0, %[value3]  # evensel \n\t"  \
     Set_EventSel("t0")       \
     "li t0, 1    \n\t"      \
-    "addi x0, t0, 12  #SetPfcEnable \n\t"  \
+    Set_PfcEnable("t0")  \
     : \
     :[value1]"r"(maxevent), [value2]"r"(warmupinst), [value3]"r"(eventsel) \
 ); 
